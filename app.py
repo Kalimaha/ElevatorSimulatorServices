0d5a6b3f2db55711aa3a19ea30a33ff105608f38
Kalimaha <guido.barbaglia@gmail.com>
2016-03-08 07:27:05 +1100

    Main script.

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 import json
 from flask import Flask
 from bson import json_util
-from fake_data_crud_service.rest.books import books
-from fake_data_crud_service.resources.schema import schema
+from elevator_service.rest.elevators import elevators
 
 
 app = Flask(__name__)
-app.register_blueprint(books, url_prefix='/books')
+app.register_blueprint(elevators, url_prefix='/elevators')
 
 
-@app.route('/', methods=['GET'])
-def json_schema_service():
-    return json.dumps(schema, sort_keys=True, indent=4, default=json_util.default), \
-           200, \
-           {'Content-Type': 'application/schema+json; charset=utf-8'}
+# @app.route('/', methods=['GET'])
+# def json_schema_service():
+#     return json.dumps(schema, sort_keys=True, indent=4, default=json_util.default), \
+#            200, \
+#            {'Content-Type': 'application/schema+json; charset=utf-8'}
 
 
 if __name__ == '__main__':
